1c72cd762f3ee2b18710f9bbba4d57e08f16b121
CookieCrumble <rndud329@gmail.com>
2023-11-22 08:50:52 +0900

    rm token

--- fairytale/appv3.py
+++ fairytale/appv3.py
@@ -1,4 +1,4 @@
-'''
+"""
 file: appv3.py
 author: Guyoung Kwon
 date: 2022.09.26
@@ -15,7 +15,7 @@ note:
     이전 버전은 end point가 하나였으나 
     end point를 2개로 확장하여 background이미지를 유저가 선택할 수 있도록 변경
     마지막 end point에서 mask이미지와 blended이미지를 전송
-'''
+"""
 from flask import Flask, request, Response
 import cv2
 import requests
@@ -28,11 +28,12 @@ from blenImage import blendImage
 
 
 app = Flask(__name__)
-app.debug=True
+app.debug = True
 
-addr_spring = 'http://192.168.0.116:5003' + '/receive/image'
-addr_colab = 'http://5371-35-203-136-123.ngrok.io' + '/gettext'
-text = ''
+addr_spring = "address" + "/receive/image"
+addr_colab = "address" + "/gettext"
+text = ""
+
 
 def decodeImage(data, name):
     # 이미지를 디코딩하고 저장
@@ -41,84 +42,88 @@ def decodeImage(data, name):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     img = cv2.resize(img, (512, 512))
 
-    cv2.imwrite('static/data/' + name + '.png', img)
-    print('saved ' + name + ' image')
+    cv2.imwrite("static/data/" + name + ".png", img)
+    print("saved " + name + " image")
+
 
 def imageBlend():
-    source_file = 'static/data/origin.png'
-    mask_file = 'static/data/mask_image.png'
-    target_file = 'static/data/back_ground.png'
+    source_file = "static/data/origin.png"
+    mask_file = "static/data/mask_image.png"
+    target_file = "static/data/back_ground.png"
     blendImage(source_file, mask_file, target_file)
     # cv2.imread('static/images/data_second_pass.png')
-    final_data = cv2.imread('static/images/data_second_pass.png')
+    final_data = cv2.imread("static/images/data_second_pass.png")
+
 
 def getMask(data):
     maskGen.create_mask(data)
-    cv2.imread('static/data/mask_image.png')
+    cv2.imread("static/data/mask_image.png")
     imageBlend()
 
-@app.route('/gettext', methods=['POST'])
+
+@app.route("/gettext", methods=["POST"])
 def getText():
     global text
-    if request.method == 'POST':
+    if request.method == "POST":
         # For json communicatoin
         if request.is_json:
             r = request.get_json()
 
-        text = r['text'][0]
+        text = r["text"][0]
         # send text data to colab flask
-        sendMessage(text, 'text', addr_colab)
+        sendMessage(text, "text", addr_colab)
 
     # decode image data and make mask image
-    data = cv2.imread('static/data/back_ground.png')
-    _, data = cv2.imencode('.png', data)
-    data = base64.encodebytes(data).decode('utf-8')
+    data = cv2.imread("static/data/back_ground.png")
+    _, data = cv2.imencode(".png", data)
+    data = base64.encodebytes(data).decode("utf-8")
 
-    response = {"image":data, "text":text}
-    
+    response = {"image": data, "text": text}
+
     # encode response
     response = jsonpickle.encode(response)
-    return Response(response=response, status = 200, mimetype='application/json')
+    return Response(response=response, status=200, mimetype="application/json")
 
-@app.route('/getimage', methods=['POST'])
+
+@app.route("/getimage", methods=["POST"])
 def getImage():
     global text
-    if request.method == 'POST':
+    if request.method == "POST":
         # For json communicatoin
         if request.is_json:
             r = request.get_json()
 
-        decodeImage(r['image'][0], 'origin')
+        decodeImage(r["image"][0], "origin")
 
         # decode image data and make mask image
-        img = cv2.imread('static/data/origin.png')
+        img = cv2.imread("static/data/origin.png")
         getMask(img)
-        data = cv2.imread('static/images/data_second_pass.png')
-        _, data = cv2.imencode('.png', data)
-        data = base64.encodebytes(data).decode('utf-8')
-      
+        data = cv2.imread("static/images/data_second_pass.png")
+        _, data = cv2.imencode(".png", data)
+        data = base64.encodebytes(data).decode("utf-8")
 
-        response = {"image":data, "text":text}
-    
+        response = {"image": data, "text": text}
+
     # encode response
     response = jsonpickle.encode(response)
-    return Response(response=response, status = 200, mimetype='application/json')
-        
+    return Response(response=response, status=200, mimetype="application/json")
+
+
 def sendMessage(data, type, addr):
     # prepare headers for http request
-    content_type = 'application/json'
-    headers = {'content-type': content_type, 'charset':'utf-8'}
-    
-    print('send text')
-    data = {"data":data}
+    content_type = "application/json"
+    headers = {"content-type": content_type, "charset": "utf-8"}
+
+    print("send text")
+    data = {"data": data}
     data = json.dumps(data)
 
     # send http request with image and receive response
     response = requests.post(addr, data=data, headers=headers)
-    
+
     # decode image
-    decodeImage(response.text, 'back_ground')
+    decodeImage(response.text, "back_ground")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
